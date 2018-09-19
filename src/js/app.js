0eaa88935bd51705a007d6880057aa1d592c9e50
AzureAD\NguyenDucTrung <trung.nguyen1@hubcba.com>
2018-09-19 15:36:45 +0700

    [https://www.w3resource.com/javascript-exercises/javascript-dom-exercises.php] Exercise 01

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,13 @@ var req = require.context("../images", true, /\.(png|svg|jpg|gif|jpeg|ico)$/);
 req.keys().forEach(function(key){
     req(key);
 });
+
+//01
+function js_style(){
+    const pText = document.querySelector("#text");
+    pText.style.fontSize = "20px";
+    pText.style.color = "yellow";
+}
+
+const jsstyle = document.querySelector("#jsstyle");
+jsstyle.addEventListener("click", js_style);
